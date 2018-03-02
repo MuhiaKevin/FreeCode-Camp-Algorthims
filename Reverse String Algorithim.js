508c75fb001d9436cd2973a8719d6a88378e4d1d
MuhiaKevin <archerkreiger@gmail.com>
2018-03-02 16:14:09 +0800

    Added the Reverse String script

---
+++ Reverse String Algorithim.js
@@ -0,0 +1,19 @@
+
+// Basic
+
+function reverseString(str) {
+  var arrray = [];
+  array = str.split('');
+  array.reverse();
+  str = array.join('');
+  
+  return str;
+}
+
+reverseString("hello");
+
+
+
+// TO DO
+
+// Rewrite the reverse inbuilt function in javscript
